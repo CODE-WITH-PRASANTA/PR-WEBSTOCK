bbc02c14334d32e24cba5fa0f35329f5dc3cbb27
SarojKumarMallik <sarojkumarmallik94@gmail.com>
2025-12-05 00:53:41 +0530

    Footer and Contact Us Page Design

--- Frontend/src/Pages/Home/Home.jsx
+++ Frontend/src/Pages/Home/Home.jsx
@@ -4,6 +4,8 @@ import News from '../../Components/News/News'
 import Homeabout from '../../Components/Homeabout/Homeabout'
 import Oursolutions from '../../Components/Oursolutions/Oursolutions'
 import Creativeshowcase from '../../Components/Creativeshowcase/Creativeshowcase'
+import Contactus from '../../Components/Contactus/Contactus'
+import Footer from '../../Components/Footer/Footer'
 
 const Home = () => {
   return (
@@ -13,6 +15,8 @@ const Home = () => {
       <Homeabout/>
       <Oursolutions/>
       <Creativeshowcase/>
+      <Contactus/>
+      <Footer/>
     </div>
   )
 }
