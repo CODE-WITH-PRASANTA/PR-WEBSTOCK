82062ec0c58fa984e5d9120ddfdab2b5d4b8aced
Prasanta Kumar Khuntia <prasantakhuntia2018@gmail.com>
2025-12-06 15:55:54 +0530

    Proper Done System content and correction

--- Frontend/src/Pages/Service/Service.jsx
+++ Frontend/src/Pages/Service/Service.jsx
@@ -2,11 +2,8 @@ import React from 'react'
 import Servicebreadcrum from '../../Components/Servicebreadcrum/Servicebreadcrum'
 import Webdesignexcellence from '../../Components/Webdesignexcellence/Webdesignexcellence'
 import Websitedesignservices from '../../Components/Websitedesignservices/Websitedesignservices'
-<<<<<<< HEAD
-import Creativewebdesign from '../../Components/Creativewebdesign/Creativewebdesign'
-=======
 import Servicesweoffer from '../../Components/Servicesweoffer/Servicesweoffer'
->>>>>>> 29245230638eb1566f0bdba0f1cac5c747763c71
+import Creativewebdesign from '../../Components/Creativewebdesign/Creativewebdesign'
 
 
 
