1f7312d7fedf440231a7537f1c7ccac48eea3f26
jaganrajaa15-arch <jaganrajaa15@gmail.com>
2026-06-18 16:03:43 +0530

    pull

--- Frontend/src/Pages/Career/Career.jsx
+++ Frontend/src/Pages/Career/Career.jsx
@@ -3,6 +3,8 @@ import CareerBreadcrum from '../../Components/CareerBreadcrum/CareerBreadcrum'
 import CareerLatestJob from '../../Components/CareerLatestJob/CareerLatestJob'
 import CareerMoreOpportunities from '../../Components/CareerMoreOpportunities/CareerMoreOpportunities'
 import CareerBlogAndArticle from '../../Components/CareerBlogAndArticle/CareerBlogAndArticle'
+import FrontendDev from '../../Components/FrontendDev/FrontendDev'
+import AccountBdmExe from '../../Components/AccountBdmExe/AccountBdmExe'
 // import CareerBrowseJob from '../../Components/CareerBrowseJob/CareerBrowseJob'
 
 const Career = () => {
@@ -12,6 +14,8 @@ const Career = () => {
       <CareerLatestJob/>
       <CareerMoreOpportunities/>
       {/* <CareerBrowseJob/> */}
+      <FrontendDev/>
+      <AccountBdmExe/>
       <CareerBlogAndArticle/>
     </div>
   )
