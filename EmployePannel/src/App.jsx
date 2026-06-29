80b0b530ed0d37344c57635ca0ec9327ce60fedb
Santanu <santanuranjanbal@gmail.com>
2026-06-29 21:11:53 +0530

    done

---
+++ EmployePannel/src/App.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
+import MainLayout from "./Layout/MainLayout/MainLayout";
+
+// Pages
+import Dashboard from "./Pages/Dashboard/Dashboard";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+
+        {/* Redirect */}
+        <Route
+          path="/"
+          element={<Navigate to="/employee/dashboard" replace />}
+        />
+
+        {/* Employee Layout */}
+        <Route path="/employee" element={<MainLayout />}>
+
+          <Route
+            path="dashboard"
+            element={<Dashboard />}
+          />
+
+          {/* Add more routes here */}
+          {/* <Route path="calendar" element={<Calendar />} /> */}
+          {/* <Route path="today-attendance" element={<TodayAttendance />} /> */}
+
+        </Route>
+
+        <Route
+          path="*"
+          element={<h1>404 Page Not Found</h1>}
+        />
+
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
