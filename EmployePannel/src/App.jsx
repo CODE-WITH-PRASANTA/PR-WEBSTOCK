70c0974569f96810a1bdebfc75d02c1fdb0fab96
Pritam barik <barickpritam08@gmail.com>
2026-06-30 16:17:19 +0530

    done

--- EmployePannel/src/App.jsx
+++ EmployePannel/src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
 import MainLayout from "./Layout/MainLayout/MainLayout";
 
 // Pages
 import Dashboard from "./Pages/Dashboard/Dashboard";
 
-// Components
 import TaskHeader from "./Components/TaskHeader/TaskHeader";
 import TaskList from "./Components/TaskList/TaskList";
 import SalaryDetails from "./Components/SalaryDetails/SalaryDetails";
@@ -14,26 +18,65 @@ import SalaryHeader from "./Components/SalaryHeader/SalaryHeader";
 import CompanyPolicies from "./Components/CompanyPolicies/CompanyPolicies";
 import TeamMembers from "./Components/TeamMembers/TeamMembers";
 
-const App = () => (
-  <BrowserRouter>
-    <Routes>
-      {/* Redirect */}
-      <Route path="/" element={<Navigate to="/employee/dashboard" replace />} />
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
 
-      {/* Employee */}
-      <Route path="/employee" element={<MainLayout />}>
-        <Route index element={<Dashboard />} />
-        <Route path="dashboard" element={<Dashboard />} />
-        <Route path="team-members" element={<TeamMembers />} />
-        <Route path="my-tasks" element={<TaskList />} />
-        <Route path="reimbursements" element={<SalaryDetails />} />
-        <Route path="company-policies" element={<CompanyPolicies />} />
-      </Route>
+        {/* Redirect */}
+        <Route
+          path="/"
+          element={<Navigate to="/employee/dashboard" replace />}
+        />
 
-      {/* 404 */}
-      <Route path="*" element={<h1>404 Page Not Found</h1>} />
-    </Routes>
-  </BrowserRouter>
-);
+        {/* Employee Layout */}
+      
+
+         
+
+          <Route path="/employee" element={<MainLayout />}>
+
+    <Route index element={<Dashboard />} />
+
+    <Route
+        path="dashboard"
+        element={<Dashboard />}
+    />
+
+    <Route
+        path="team-members"
+        element={<TeamMembers />}
+    />
+
+    <Route
+        path="tasklist"
+        element={<TaskList />}
+    />
+
+    <Route
+        path="salary"
+        element={<SalaryDetails />}
+    />
+
+    <Route
+        path="policies"
+        element={<CompanyPolicies />}
+    />
+
+</Route>
+
+        
+
+        <Route
+          path="*"
+          element={<h1>404 Page Not Found</h1>}
+        />
+         
+        
+        
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
